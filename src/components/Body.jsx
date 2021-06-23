f480dc05170ea8cbe2c6986c3ea66350cf7368e2
Bryan Eduria <dev.bryeduria@gmail.com>
2021-06-23 12:27:42 +0800

    fixed image alignment

---
+++ src/components/Body.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import Header from "./Header";
+import SectionRoot from "./SectionRoot";
+
+class Body extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <SectionRoot />
+      </>
+    );
+  }
+}
+
+export default Body;
