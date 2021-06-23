9f814798f0b77ac59496aca4e29fec7333297564
Bryan Eduria <dev.bryeduria@gmail.com>
2021-06-23 11:22:00 +0800

    updated navbar and title

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,27 @@
-import { Component } from 'react';
-import NavLinks from './NavLinks';
+import { Component } from "react";
+import NavLinks from "./NavLinks";
 
 class Nav extends Component {
-    render() {
-        return <nav class="font-sans flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full">
-            <div class="mb-2 sm:mb-0 inner">
-                <a class="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold">ALTYKIN</a><br />
-                <span class="text-xs text-grey-dark">Bug Hunter. Software Developer.</span>
-            </div>
+  render() {
+    return (
+      <nav className="font-sans flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full">
+        <div className="mb-2 sm:mb-0 inner">
+          <a
+            href="/"
+            className="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold"
+          >
+            ALTYKIN
+          </a>
+          <br />
+          <span className="text-xs text-grey-dark">
+            Bug Hunter. Software Developer.
+          </span>
+        </div>
 
-            <NavLinks />
-        </nav>
-    }
+        <NavLinks />
+      </nav>
+    );
+  }
 }
-
 
 export default Nav;
