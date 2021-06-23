27461922fddda139754bce63f60fee5c78c46aeb
Bryan Eduria <dev.bryeduria@gmail.com>
2021-06-23 13:19:51 +0800

    fixed class names

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,18 +1,30 @@
-import { Component } from 'react';
+import { Component } from "react";
 
 class NavLinks extends Component {
-
-    render() {
-        return (
-            <div class="sm:mb-0 self-center">
-                <a href="#whoami" class="text-md no-underline text-black hover:text-blue-dark ml-2 px-1">whoami</a>
-                <a href="#my_projects" class="text-md no-underline text-grey-darker hover:text-blue-dark ml-2 px-1">my_projects</a>
-                <a href="#my_skills" class="text-md no-underline text-grey-darker hover:text-blue-dark ml-2 px-1">my_skills</a>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="sm:mb-0 self-center">
+        <a
+          href="#whoami"
+          className="text-md no-underline text-black hover:text-blue-dark ml-2 px-1"
+        >
+          whoami
+        </a>
+        <a
+          href="#my_projects"
+          className="text-md no-underline text-grey-darker hover:text-blue-dark ml-2 px-1"
+        >
+          my_projects
+        </a>
+        <a
+          href="#my_skills"
+          className="text-md no-underline text-grey-darker hover:text-blue-dark ml-2 px-1"
+        >
+          my_skills
+        </a>
+      </div>
+    );
+  }
 }
-
-
 
 export default NavLinks;
